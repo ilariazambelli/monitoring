8b4bdd3bd5e6b5dd86bdfa37d79efca6e85fcce8
ilariazambelli <ilariazambellii@gmail.com>
2025-05-06 12:43:34 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -7,3 +7,11 @@ setwd("C:/Users/Ilaria/OneDrive - Alma Mater Studiorum Università di Bologna/De
 
 mato = rast("matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+
+#Bands:
+#band1 = near infrared
+#band2 = red
+#band3 = green
+
+plotRGB(mato, r=2, g=3, b=1) #vegetazione blu
+plotRGB(mato, r=2, g=1, b=3) #vegetazione verde
